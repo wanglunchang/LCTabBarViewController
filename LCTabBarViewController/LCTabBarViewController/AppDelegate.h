b4189a4a359ddc986d52f0d4e1fbb8942d420dcc
WangLunChang <523925385@qq.com>
2016-07-27 15:10:04 +0800

    tabBar submit

---
+++ LCTabBarViewController/LCTabBarViewController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LCTabBarViewController
+//
+//  Created by mac on 16/7/27.
+//  Copyright © 2016年 Mac.person. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LCTabBarViewController/LCTabBarViewController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LCTabBarViewController
+//
+//  Created by mac on 16/7/27.
+//  Copyright © 2016年 Mac.person. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
